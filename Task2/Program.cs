f5fbc798f8ee6ca8c66df8d4b6c8ad3cf06d1996
Baktiyar <dudewithsurvey@gmail.com>
2023-07-23 22:09:16 +0600

    Task2 finished

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,17 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("Enter a number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) 
+{
+    Console.Write($"{number1} is bigger and {number2} is smaller");
+}
+else
+{
+    Console.Write($"{number2} is bigger and {number1} is smaller");
+}
