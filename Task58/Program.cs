b67ed63d242eb5dba8767a936afaf4c5b806129f
Baktiyar <dudewithsurvey@gmail.com>
2023-09-04 10:48:55 +0600

    Finished

---
+++ Task58/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.WriteLine("Enter the size of matrices, and range of random integers:");
+int m = InputNumbers("Enter the number of line of first matrix: ");
+int n = InputNumbers("Enter the number of columns of first matrix (and number of lines of second matrix): ");
+int p = InputNumbers("Enter the number of columns second matrix: ");
+int range = InputNumbers("Range from 1 to: ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"First matrix:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Second matrix:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m, p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine("Multiplication of the two matrices:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum = sum + firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
