3bf24ee4c662756ce3b451ccd6a4540d19e97f8d
Baktiyar <dudewithsurvey@gmail.com>
2023-09-04 11:48:43 +0600

    Finished1

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -5,13 +5,13 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
 
-Console.WriteLine("Enter the size of array x and y:");
-Console.Write("x: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("y: ");
-int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the size of array m and n:");
+Console.Write("m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] array = new int[x, y];
+int[,] array = new int[m, n];
 CreateArray(array);
 WriteArray(array);
 
@@ -21,14 +21,14 @@ ArithmeticMean(array);
 
 void ArithmeticMean(int[,] arr)
 {
-    for (int i = 0; i < y; i++)
+    for (int i = 0; i < n; i++)
     {
         double arithmeticMean = 0;
-        for (int j = 0; j < x; j++)
+        for (int j = 0; j < m; j++)
         {
             arithmeticMean = arithmeticMean + array[j, i];
         }
-        arithmeticMean = Math.Round(arithmeticMean / x, 1);
+        arithmeticMean = Math.Round(arithmeticMean / m, 1);
         Console.WriteLine($"{i + 1} column: {arithmeticMean}");
     }
 }
